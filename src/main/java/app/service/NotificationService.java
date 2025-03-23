a3799caabe1b4c79ece1114f34b9e160f34301eb
Stefan Panaytov <StefoPanayotov@gmail.com>
2025-03-23 22:37:59 +0200

    THe first commit for my notification microservice

---
+++ src/main/java/app/service/NotificationService.java
@@ -0,0 +1,154 @@
+package app.service;
+
+import app.model.NotificationStatus;
+import app.model.NotificationType;
+import app.model.Notification;
+import app.model.NotificationPreference;
+import app.repository.NotificationPreferenceRepository;
+import app.repository.NotificationRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+import app.web.dto.*;
+import app.web.mapper.DtoMapper;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class NotificationService {
+
+    private final NotificationPreferenceRepository preferenceRepository;
+    private final NotificationRepository notificationRepository;
+    private final MailSender mailSender;
+
+    @Autowired
+    public NotificationService(NotificationPreferenceRepository preferenceRepository, NotificationRepository notificationRepository, MailSender mailSender) {
+        this.preferenceRepository = preferenceRepository;
+        this.notificationRepository = notificationRepository;
+        this.mailSender = mailSender;
+    }
+
+    public NotificationPreference upsertPreference(UpsertNotificationPreference dto) {
+
+        Optional<NotificationPreference> userNotificationPreferenceOptional = preferenceRepository.findByUserId(dto.getUserId());
+
+        if (userNotificationPreferenceOptional.isPresent()) {
+            NotificationPreference preference = userNotificationPreferenceOptional.get();
+            preference.setContactInfo(dto.getContactInfo());
+            preference.setEnabled(dto.isNotificationEnabled());
+            preference.setType(DtoMapper.fromNotificationTypeRequest(dto.getType()));
+            preference.setUpdatedOn(LocalDateTime.now());
+            return preferenceRepository.save(preference);
+        }
+
+        NotificationPreference notificationPreference = NotificationPreference.builder()
+                .userId(dto.getUserId())
+                .type(DtoMapper.fromNotificationTypeRequest(dto.getType()))
+                .enabled(dto.isNotificationEnabled())
+                .contactInfo(dto.getContactInfo())
+                .createdOn(LocalDateTime.now())
+                .updatedOn(LocalDateTime.now())
+                .build();
+
+        return preferenceRepository.save(notificationPreference);
+    }
+
+    public NotificationPreference getPreferenceByUserId(UUID userId) {
+
+        return preferenceRepository.findByUserId(userId).orElseThrow(() -> new NullPointerException("Notification preference for user id %s was not found.".formatted(userId)));
+    }
+
+    public Notification sendNotification(NotificationRequest notificationRequest) {
+
+        UUID userId = notificationRequest.getUserId();
+        NotificationPreference userPreference = getPreferenceByUserId(userId);
+
+        if (!userPreference.isEnabled()) {
+            throw new IllegalArgumentException("User with id %s does not allow to receive notifications.".formatted(userId));
+        }
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(userPreference.getContactInfo());
+        message.setSubject(notificationRequest.getSubject());
+        message.setText(notificationRequest.getBody());
+
+        // Entity building
+        Notification notification = Notification.builder()
+                .subject(notificationRequest.getSubject())
+                .body(notificationRequest.getBody())
+                .createdOn(LocalDateTime.now())
+                .userId(userId)
+                .deleted(false)
+                .type(NotificationType.EMAIL)
+                .build();
+
+        try {
+            mailSender.send(message);
+            notification.setStatus(NotificationStatus.SUCCEEDED);
+        } catch (Exception e) {
+            notification.setStatus(NotificationStatus.FAILED);
+            log.warn("There was an issue sending an email to %s due to %s.".formatted(userPreference.getContactInfo(), e.getMessage()));
+        }
+
+        return notificationRepository.save(notification);
+    }
+
+    public List<Notification> getNotificationHistory(UUID userId) {
+
+        return notificationRepository.findAllByUserIdAndDeletedIsFalse(userId);
+    }
+
+    public NotificationPreference changeNotificationPreference(UUID userId, boolean enabled) {
+
+        // If exist - return NotificationPreference
+        // If does not exist - throws exception
+        NotificationPreference notificationPreference = getPreferenceByUserId(userId);
+        notificationPreference.setEnabled(enabled);
+        return preferenceRepository.save(notificationPreference);
+    }
+
+    public void clearNotifications(UUID userId) {
+
+        List<Notification> notifications = getNotificationHistory(userId);
+
+        notifications.forEach(notification -> {
+            notification.setDeleted(true);
+            notificationRepository.save(notification);
+        });
+    }
+
+    public void retryFailedNotifications(UUID userId) {
+
+        NotificationPreference userPreference = getPreferenceByUserId(userId);
+        if (!userPreference.isEnabled()) {
+            throw new IllegalArgumentException("User with id %s does not allow to receive notifications.".formatted(userId));
+        }
+
+        List<Notification> failedNotifications = notificationRepository.findAllByUserIdAndStatus(userId, NotificationStatus.FAILED);
+        failedNotifications = failedNotifications.stream().filter(notification ->  !notification.isDeleted()).toList();
+
+        for (Notification notification : failedNotifications) {
+
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(userPreference.getContactInfo());
+            message.setSubject(notification.getSubject());
+            message.setText(notification.getBody());
+
+            try {
+                mailSender.send(message);
+                notification.setStatus(NotificationStatus.SUCCEEDED);
+            } catch (Exception e) {
+                notification.setStatus(NotificationStatus.FAILED);
+                log.warn("There was an issue sending an email to %s due to %s.".formatted(userPreference.getContactInfo(), e.getMessage()));
+            }
+
+            notificationRepository.save(notification);
+        }
+    }
+}
---
+++ src/main/java/app/web/NotificationController.java
@@ -0,0 +1,113 @@
+package app.web;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import app.model.Notification;
+import app.model.NotificationPreference;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import app.service.NotificationService;
+import app.web.dto.NotificationPreferenceResponse;
+import app.web.dto.NotificationRequest;
+import app.web.dto.NotificationResponse;
+import app.web.dto.UpsertNotificationPreference;
+import app.web.mapper.DtoMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/notifications")
+@Tag(name = "Notification Management", description = "Operations related to notifications")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @Autowired
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @Operation(summary = "Create new Notification Preference", description = "Returns the created notification preference.")
+    @PostMapping("/preferences")
+    public ResponseEntity<NotificationPreferenceResponse> upsertNotificationPreference(@RequestBody UpsertNotificationPreference upsertNotificationPreference) {
+
+        NotificationPreference notificationPreference = notificationService.upsertPreference(upsertNotificationPreference);
+
+        NotificationPreferenceResponse responseDto = DtoMapper.fromNotificationPreference(notificationPreference);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(responseDto);
+    }
+
+    @GetMapping("/preferences")
+    public ResponseEntity<NotificationPreferenceResponse> getUserNotificationPreference(@RequestParam(name = "userId") UUID userId) {
+
+        NotificationPreference notificationPreference = notificationService.getPreferenceByUserId(userId);
+
+        NotificationPreferenceResponse responseDto = DtoMapper.fromNotificationPreference(notificationPreference);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(responseDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<NotificationResponse> sendNotification(@RequestBody NotificationRequest notificationRequest) {
+
+        // Entity
+        Notification notification = notificationService.sendNotification(notificationRequest);
+
+        // DTO
+        NotificationResponse response = DtoMapper.fromNotification(notification);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<NotificationResponse>> getNotificationHistory(@RequestParam(name = "userId") UUID userId) {
+
+        List<NotificationResponse> notificationHistory = notificationService.getNotificationHistory(userId).stream().map(DtoMapper::fromNotification).toList();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(notificationHistory);
+    }
+
+    @PutMapping("/preferences")
+    public ResponseEntity<NotificationPreferenceResponse> changeNotificationPreference(@RequestParam(name = "userId") UUID userId, @RequestParam(name = "enabled") boolean enabled) {
+
+        NotificationPreference notificationPreference = notificationService.changeNotificationPreference(userId, enabled);
+
+        NotificationPreferenceResponse responseDto = DtoMapper.fromNotificationPreference(notificationPreference);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(responseDto);
+    }
+
+    // DELETE /api/v1/notifications
+    @DeleteMapping
+    public ResponseEntity<Void> clearNotificationHistory(@RequestParam(name = "userId") UUID userId) {
+
+        notificationService.clearNotifications(userId);
+
+        return ResponseEntity.ok().body(null);
+    }
+
+
+    @PutMapping
+    public ResponseEntity<Void> retryFailedNotifications(@RequestParam(name = "userId") UUID userId) {
+
+        notificationService.retryFailedNotifications(userId);
+
+        return ResponseEntity.ok().body(null);
+    }
+}
